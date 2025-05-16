49876cbeb4efc3613ee539fed6dfe8210a1b74e8
Janos Imhof <janos.imhof@yetnet.ch>
2025-05-16 18:40:07 +0200

    init basic blog component & setup.md

---
+++ src/app/components/blog/blog.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'blog-component',
+  templateUrl: './blog.component.html',
+  styleUrl: './blog.component.scss'
+})
+export class BlogComponent {
+
+}
