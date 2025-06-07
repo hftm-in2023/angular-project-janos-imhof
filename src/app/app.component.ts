f49c3b846cc293c1a11d3b96e471148992db2340
Janos Imhof <janos.imhof@yetnet.ch>
2025-06-07 08:27:39 +0200

    chore: add angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MatSlideToggle } from '@angular/material/slide-toggle';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggle],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-project-janos-imhof';
