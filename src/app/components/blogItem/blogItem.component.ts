651956f09c2e2f510306fa2b07a7bbe6c4f17b32
Janos Imhof <janos.imhof@yetnet.ch>
2025-06-18 18:52:41 +0200

    feat(blog): add random images to blog post and proxy config to ngServe

--- src/app/components/blogItem/blogItem.component.ts
+++ src/app/components/blogItem/blogItem.component.ts
@@ -3,18 +3,19 @@ import { Component, Input } from '@angular/core';
 //material
 import { MatCardModule } from '@angular/material/card';
 import { BlogItem } from '../../types/blogItem';
-import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatIconButton } from '@angular/material/button';
 import { formatDate } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-blog-item',
   standalone: true,
-  imports: [MatCardModule, MatButton, MatIcon, MatIconButton],
+  imports: [MatCardModule, MatIcon, MatIconButton],
   templateUrl: './blogItem.component.html',
   styleUrls: ['./blogItem.component.scss'],
 })
 export class BlogItemComponent {
   @Input() blogItem!: BlogItem;
   protected readonly formatDate = formatDate;
+  protected readonly Math = Math;
 }
